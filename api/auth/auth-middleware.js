8b761f1c134806e51e6cebc494fc3845a09012c6
ferengitooth <ehitsamill@gmail.com>
2021-09-15 00:24:43 -0700

    finished auth middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -25,7 +25,7 @@ async function checkUsernameFree(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username})
     if(!users.length) next()
-    else next({ "message": "Username taken" })
+    else next({ message: "Username taken", status: 422 })
   } catch(err) {
     next(err)
   }
@@ -42,8 +42,8 @@ async function checkUsernameFree(req, res, next) {
 async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username})
-    if(!users.length) next()
-    else next({ message: "Username taken", status: 422 })
+    if(users.length) next()
+    else next({ message: "Invalid credentials", status: 401 })
   } catch(err) {
     next(err)
   }
@@ -58,7 +58,11 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422 })
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
